0ab18354409642fede6613acc14ed41392108238
irem <ekmekciirem9@gmail.com>
2025-12-09 17:16:14 +0300

    done

--- travelapp/Controllers/HomeController.cs
+++ travelapp/Controllers/HomeController.cs
@@ -1,31 +1,31 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using travelapp.Models;
 
-namespace travelapp.Controllers;
-
-public class HomeController : Controller
+namespace travelapp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+       
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About TravelApp";
+            ViewBag.Description = "TravelApp, Ankara, İstanbul, İzmir ve Sakarya şehirlerindeki turları ve etkinlikleri listeleyen basit bir gezi planlama uygulamasıdır.";
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
